eb0448d375ca125c3823d89d06492ea05b588cfb
nghivu29 <56593821+nghivu29@users.noreply.github.com>
2022-09-18 18:23:33 +0700

    chat group demo

---
+++ TCP_Private_Client/TCP_Private_Client/Form_NewGroup.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TCP_Private_Client
+{
+    public partial class Form_NewGroup : Form
+    {
+        private Form_Main formMain;
+        
+        private string[] users;
+        
+        public Form_NewGroup(string[] users, Form_Main formMain)
+        {
+            this.formMain = formMain;
+            InitializeComponent();
+            // init list box user online
+            this.users = users;
+            InitListBoxUserOnline(this.users);
+        }
+
+        private void InitListBoxUserOnline(string[] users)
+        {
+            foreach (var user in users)
+            {
+                listBoxUserOnline.Items.Add(user);
+            }
+        }
+
+        private void listBoxUserOnline_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // throw new System.NotImplementedException();
+        }
+
+        private void Form_NewGroup_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            var selectedItems = listBoxUserOnline.SelectedItems;
+            if (selectedItems == null || selectedItems.Count == 0)
+                return;
+
+            foreach (var item in selectedItems)
+            {
+                this.listBoxGroupMember.Items.Add(item.ToString());
+            }
+
+            for (int i = selectedItems.Count - 1; i >= 0; i--)
+            {
+                this.listBoxUserOnline.Items.Remove(selectedItems[i]);
+            }
+        }
+        
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            var selectedItems = listBoxGroupMember.SelectedItems;
+            if (selectedItems == null || selectedItems.Count == 0)
+                return;
+            
+            foreach (var item in selectedItems)
+            {
+                this.listBoxUserOnline.Items.Add(item);
+            }
+            
+            for (int i = selectedItems.Count - 1; i >= 0; i--)
+            {
+                this.listBoxGroupMember.Items.Remove(selectedItems[i]);
+            }
+        }
+
+        private void buttonDone_Click(object sender, EventArgs e)
+        {
+            //todo: request to server create new group
+            string groupName = textBoxGroupName.Text;
+            string members = string.Join(",", this.listBoxGroupMember.Items.OfType<string>().ToArray());
+
+            string data = String.Format("NEWGROUP|Name={0},Members=[{1}]", groupName, members);
+            formMain.SendData(data); // NewGroup|Name=name,MembersId=['1','2','3']
+            // textBoxGroupName.Text = data; // test code
+            
+            //close window
+            this.Close();
+        }
+    }
+}
---
+++ TCP_Private_Client/TCP_Private_Client/Helper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TCP_Private_Client
+{
+    public static class Helper
+    {
+        public static string ParseAttribute(string data, string key)
+        {
+            if (string.IsNullOrEmpty(data) || string.IsNullOrEmpty(key))
+                return "";
+            
+            if (data.Contains(key+"="))
+            {
+                var i1 = data.IndexOf(key+"=") + key.Length+1;
+                var sub= data.Substring(i1);
+                var i2 =  (sub[0] != '[') ? sub.IndexOf(',') : sub.Length  ;
+                var name = (i2 != -1) ? sub.Substring(0, i2) : sub;
+                return name;
+            }
+            return "";
+        }
+
+        public static string[] ParseArray(string data)
+        {
+            if (string.IsNullOrEmpty(data)) return Array.Empty<string>();
+            if (data[0] == '[') data = data.Remove(0, 1);
+            if (data[data.Length-1] == ']') data = data.Remove(data.Length - 1, 1);
+            if (string.IsNullOrEmpty(data)) return Array.Empty<string>();
+            return data.Split(',');
+        }
+    }
+}
